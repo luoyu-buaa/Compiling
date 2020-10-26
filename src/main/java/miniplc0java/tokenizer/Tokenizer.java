8a9f4a9241fca36b93ddd391b7de161d72e256e3
luoyuxxx <53784453+luoyuxxx@users.noreply.github.com>
2020-10-27 00:47:27 +0800

    Update Tokenizer.java

--- src/main/java/miniplc0java/tokenizer/Tokenizer.java
+++ src/main/java/miniplc0java/tokenizer/Tokenizer.java
@@ -1,8 +1,8 @@
 package miniplc0java.tokenizer;
 
 import miniplc0java.error.TokenizeError;
-import miniplc0java.error.ErrorCode;
 import miniplc0java.util.Pos;
+import miniplc0java.error.ErrorCode;
 
 public class Tokenizer {
 
@@ -47,20 +47,32 @@ public class Tokenizer {
         // 解析成功则返回无符号整数类型的token，否则返回编译错误
         //
         // Token 的 Value 应填写数字的值
-        String token = "";
-        Pos startpos1 = it.currentPos();
-        while (Character.isDigit(it.peekChar())) {
-            token += it.nextChar();
-        }
-        Pos endpos1 = it.currentPos();
-        token = removeZero(token);
-        if (token.equals("")) {
-            token = "0";
-        }
-        int num = Integer.parseInt(token);
-        return new Token(TokenType.Uint,num,startpos1,endpos1);
+    	Pos start = it.currentPos();
+    	Pos end;
+    	char peek = it.peekChar();
+    	String strvalue = "";
+    	while(Character.isDigit(peek)) {
+    		strvalue += it.nextChar();
+    		peek = it.peekChar();
+    	}
+    	end = it.currentPos();
+    	strvalue = removeZero(strvalue);
+    	//int value = Integer.valueOf(strvalue).intValue();
+    	int value = Integer.parseInt(strvalue);
+    	Token result = new Token(TokenType.Uint, value, start, end);
+    	return result;
     }
-
+    
+    private String removeZero(String str) {
+		int len = str.length(), i = 0;
+		while (i < len && str.charAt(i) == '0') {
+			i++;
+		}
+		if (i == len)// 
+			return "0";
+		return str.substring(i);
+	}
+    
     private Token lexIdentOrKeyword() throws TokenizeError {
         // 请填空：
         // 直到查看下一个字符不是数字或字母为止:
@@ -71,58 +83,71 @@ public class Tokenizer {
         // -- 否则，返回标识符
         //
         // Token 的 Value 应填写标识符或关键字的字符串
-        String token = "";
-        Pos startpos = it.currentPos();
-        while (Character.isAlphabetic(it.peekChar()) || Character.isDigit(it.peekChar())) {
-            token +=it.nextChar();
-        }
-        Pos endpos = it.currentPos();
-        switch (token) {
-            case "begin":
-                return new Token(TokenType.Begin, token, startpos, endpos);
-            case "end":
-                return new Token(TokenType.End, token, startpos, endpos);
-            case "var":
-                return new Token(TokenType.Var, token, startpos, endpos);
-            case "const":
-                return new Token(TokenType.Const, token, startpos, endpos);
-            case "print":
-                return new Token(TokenType.Print, token, startpos, endpos);
-            default:
-                return new Token(TokenType.Ident, token, startpos, endpos);
-        }
+    	Pos start = it.currentPos();
+    	Pos end;
+    	char peek = it.peekChar();
+    	String strvalue = "";
+    	while(Character.isDigit(peek) || Character.isAlphabetic(peek)) {
+    		strvalue += it.nextChar();
+    		peek = it.peekChar();
+    	}     
+    	end = it.currentPos();
+    	Token result;
+    	switch (strvalue) {
+    	case "begin":
+    		result = new Token(TokenType.Begin,"begin",start,end);
+    		break;
+    	case "end":
+    		result = new Token(TokenType.End,"end",start,end);
+    		break;
+    	case "var":
+    		result = new Token(TokenType.Var,"var",start,end);
+    		break;
+    	case "const":
+    		result = new Token(TokenType.Const,"const",start,end);
+    		break;
+    	case "print":
+    		result = new Token(TokenType.Print,"print",start,end);
+    		break;
+    	default:
+    		result = new Token(TokenType.Ident,strvalue,start,end);
+    	}
+    	return result;
     }
+
 
     private Token lexOperatorOrUnknown() throws TokenizeError {
         switch (it.nextChar()) {
             case '+':
                 return new Token(TokenType.Plus, '+', it.previousPos(), it.currentPos());
-
+                
             case '-':
                 // 填入返回语句
-                return new Token(TokenType.Minus,'-',it.previousPos(),it.currentPos());
+            	return new Token(TokenType.Minus, '-', it.previousPos(), it.currentPos());
 
             case '*':
                 // 填入返回语句
-                return  new Token(TokenType.Mult,'*',it.previousPos(),it.currentPos());
+            	return new Token(TokenType.Mult, '*', it.previousPos(), it.currentPos());
 
             case '/':
                 // 填入返回语句
-                return  new Token(TokenType.Div,'/',it.previousPos(),it.currentPos());
+            	return new Token(TokenType.Div, '/', it.previousPos(), it.currentPos());
+
             // 填入更多状态和返回语句
             case '=':
-                // 填入返回语句
-                return  new Token(TokenType.Equal,'=',it.previousPos(),it.currentPos());
+            	return new Token(TokenType.Equal, '=', it.previousPos(), it.currentPos());
+            
+            // 填入更多状态和返回语句
             case ';':
-                // 填入返回语句
-                return  new Token(TokenType.Semicolon,';',it.previousPos(),it.currentPos());
+                return new Token(TokenType.Semicolon, ';', it.previousPos(), it.currentPos());
+                	
+            // 填入更多状态和返回语句
             case '(':
-                // 填入返回语句
-                return  new Token(TokenType.LParen,'(',it.previousPos(),it.currentPos());
+            	return new Token(TokenType.LParen, '(', it.previousPos(), it.currentPos());
+            // 填入更多状态和返回语句
             case ')':
-                // 填入返回语句
-                return  new Token(TokenType.RParen,')',it.previousPos(),it.currentPos());
-            default:
+            	return new Token(TokenType.RParen, ')', it.previousPos(), it.currentPos());
+		default:
                 // 不认识这个输入，摸了
                 throw new TokenizeError(ErrorCode.InvalidInput, it.previousPos());
         }
@@ -132,13 +157,5 @@ public class Tokenizer {
         while (!it.isEOF() && Character.isWhitespace(it.peekChar())) {
             it.nextChar();
         }
-    }
-    private String removeZero(String str) {
-        int len = str.length();
-        int i = 0;
-        while (i < len && str.charAt(i) == '0') {
-            i++;
-        }
-        return str.substring(i);
     }
 }
